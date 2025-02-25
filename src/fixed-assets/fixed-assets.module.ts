beaec7b7a0d5a410127e16dc5ae3926ae40036b5
Fernando-Leon <leon.monroy.fernando@gmail.com>
2025-02-25 12:29:42 -0600

    delete trashs

--- src/fixed-assets/fixed-assets.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FixedAssetsService } from './fixed-assets.service';
-import { FixedAssetsController } from './fixed-assets.controller';
-
-@Module({
-  controllers: [FixedAssetsController],
-  providers: [FixedAssetsService],
-})
-export class FixedAssetsModule {}

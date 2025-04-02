6e11bb15d12fdc27e2fc164da6dc3a1c19d0d84c
Fernando-Leon <leon.monroy.fernando@gmail.com>
2025-04-02 17:27:33 -0600

    New release 4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  /* otras opciones de configuración aquí */
 };
 
 export default nextConfig;
